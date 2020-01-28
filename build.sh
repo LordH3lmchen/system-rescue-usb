3dfed3612cae93ac2285b079e05a1501f852cba4
Florian Trabauer <florian@trabauer.com>
2020-01-29 00:26:40 +0100

    changed filename and ISO Label

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e -u
 
-iso_name=d4rk-helmet-linux
-iso_label="D4RK_$(date +%Y%m)"
+iso_name=eilish-linux
+iso_label="EILISH_$(date +%Y%m)"
 iso_publisher="Florian Trabauer <http://www.archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
